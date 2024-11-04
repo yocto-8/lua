d23bb8c98405f65b5c5a258f03731307adecc168
asu <sdelang@sdelang.fr>
2024-11-04 15:58:03 +0100

    Hack away version check

--- lauxlib.cpp
+++ lauxlib.cpp
@@ -940,12 +940,12 @@ static int panic (lua_State *L) {
 
 
 LUALIB_API void luaL_checkversion_ (lua_State *L, lua_Number ver) {
-  const lua_Number *v = lua_version(L);
-  if (v != lua_version(NULL))
-    luaL_error(L, "multiple Lua VMs detected");
-  else if (*v != ver)
-    luaL_error(L, "version mismatch: app. needs %f, Lua core provides %f",
-                  ver, *v);
+  // const lua_Number *v = lua_version(L);
+  // if (v != lua_version(NULL))
+  //   luaL_error(L, "multiple Lua VMs detected");
+  // else if (*v != ver)
+  //   luaL_error(L, "version mismatch: app. needs %f, Lua core provides %f",
+  //                 ver, *v);
   /* check conversions number -> integer types */
   // lua_pushnumber(L, -(lua_Number)0x1234);
   // if (lua_tointeger(L, -1) != -0x1234 ||
