fa0610859ef9e1ca027190a83a06976f8ef8065d
asu <sdelang@sdelang.fr>
2024-11-19 17:11:10 +0100

    Fix assertion failure from using peek ops

--- lcode.cpp
+++ lcode.cpp
@@ -726,7 +726,7 @@ static void codearith (FuncState *fs, OpCode op,
   if (constfolding(op, e1, e2))
     return;
   else {
-    int o2 = (op != OP_UNM && op != OP_LEN && op != OP_BNOT) ? luaK_exp2RK(fs, e2) : 0;
+    int o2 = (op != OP_UNM && op != OP_LEN && op != OP_BNOT && op != OP_PEEK && op != OP_PEEK2 && op != OP_PEEK4) ? luaK_exp2RK(fs, e2) : 0;
     int o1 = luaK_exp2RK(fs, e1);
     if (o1 > o2) {
       freeexp(fs, e1);
